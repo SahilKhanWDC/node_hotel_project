69683796bbe04eca2a9d83add7ab59d0f8103f64
Sahil khan <sk.sahilkhan543@gmail.com>
2024-07-29 18:56:43 +0530

    shifted mongodb to online server

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-const mongoUrl = "mongodb://127.0.0.1:27017/hotels"
+// const mongoURL = "mongodb://127.0.0.1:27017/hotels"
+const mongoURL = process.env.MONGODB_URL
 
-mongoose.connect(mongoUrl, {
+mongoose.connect(mongoURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config()
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -16,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("Hello! welocome to the hotel, how can i help you");
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log("App listening on port 3000");
 });
